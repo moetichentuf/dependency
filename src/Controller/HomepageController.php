c40744ae28292dba5ad5bd6a4e415a1937894f22
moeti <moetichentuf@hotmail.com>
2020-03-04 17:00:04 +0100

    added transform file

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -10,10 +10,12 @@ class HomepageController extends AbstractController
     /**
      * @Route("/homepage", name="homepage")
      */
+
     public function index()
     {
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
     }
+    
 }
---
+++ src/Entity/transform.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ModelRepository")
+ */
+class transform
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function transform(): string
+    {
+        return $this->id;
+    }
+}
+class capitalize {
+
+
+
+
+}
+
+class dash {
+
+
+
+
+}
