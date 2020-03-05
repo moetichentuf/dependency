f09dd414af53baf68a7990fa6e60cf5bdbc7f888
moeti <moetichentuf@hotmail.com>
2020-03-05 09:38:01 +0100

    implementing the transform method

--- src/Entity/transform.php
+++ src/Entity/transform.php
@@ -1,36 +1,49 @@
 <?php
+namespace Common\ContentBundle\DataTransformer;
 
-namespace App\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Form\DataTransformerInterface;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ModelRepository")
+ * Class MarkdownTransformer
+ * @package Common\ContentBundle\DataTransformer
  */
-class transform
+class transform implements DataTransformerInterface
 {
+
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * Transforms the HTML string back to markdown.
+     *
+     * @param string $val
+     * @return string
      */
-    private $id;
 
-    public function transform(): string
+    // Using this code as a base example
+
+    public function transform($val)
     {
-        return $this->id;
+        // strong to double asterisks:
+        $val = preg_replace("/\<strong\>(.+?)\<\/strong\>/i", "**$1**", $val);
+
+        // italic to double underscores:
+        $val = preg_replace("/\<em\>(.+?)\<\/em\>/i", "__$1__", $val);
+
+        return $val;
     }
-}
-class capitalize {
 
+    /**
+     * Transforms the markdown string to html.
 
+     * @param  string $val
+     * @return string
+     */
+    public function reverseTransform($val)
+    {
+        // double asterisks to strong:
+        $val = preg_replace("/\*\*(.+?)\*\*/i", "<strong>$1</strong>", $val);
 
+        // double underscores to italic:
+        $val = preg_replace("/\_\_(.+?)\_\_/i", "<em>$1</em>", $val);
 
-}
-
-class dash {
-
-
-
-
+        return $val;
+    }
 }
